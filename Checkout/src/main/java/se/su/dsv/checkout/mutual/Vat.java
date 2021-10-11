be5f28e910b3ff8b763f89fcd8a8e899293dbd95
verafied <verasolnygren@gmail.com>
2021-10-11 17:17:13 +0200

    New maven project to fix junit dependency

--- Checkout/src/main/java/se/su/dsv/checkout/mutual/Vat.java
+++
@@ -1,7 +0,0 @@
-package se.su.dsv.checkout.mutual;
-
-public interface Vat {
-
-    double getVAT();
-
-}
--- Checkout/src/test/java/se/su/dsv/checkout/mutualTest/PantTest.java
+++
@@ -1,7 +0,0 @@
-package se.su.dsv.checkout.mutualTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PantTest {
-
-}
