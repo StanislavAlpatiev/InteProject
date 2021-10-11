04e265bf29781d159078a346c02a51ac657f493d
verafied <verasolnygren@gmail.com>
2021-10-11 22:11:30 +0200

    removed access modifiers in test as they are somewhat meaningless

--- RegisterSystem/src/test/java/se/su/dsv/RegisterSystem/MoneyTest.java
+++ RegisterSystem/src/test/java/se/su/dsv/RegisterSystem/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
-public class MoneyTest {
+class MoneyTest {
 
     private static final String FAKE_CURRENCY = "NOK";
     private static final BigDecimal DEFAULT_AMOUNT = new BigDecimal(10);
@@ -15,21 +15,21 @@ public class MoneyTest {
     private static final BigDecimal ZERO_AMOUNT = new BigDecimal(0);
 
     @Test
-    public void constructorValidParameterTest() {
+    void constructorValidParameterTest() {
         Money money = new Money(DEFAULT_AMOUNT, Currency.USD);
         assertEquals(DEFAULT_AMOUNT, money.getAmount());
         assertEquals(money.getCurrency(), Currency.USD);
     }
 
     @Test
-    public void constructorThrowsIAEWhenCurrencyIsNullTest() {
+    void constructorThrowsIAEWhenCurrencyIsNullTest() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Money(DEFAULT_AMOUNT, null);
         });
     }
 
     @Test
-    public void constructorNegativeAmountTest() {
+    void constructorNegativeAmountTest() {
 
         assertThrows(IllegalArgumentException.class, () -> {
             new Money(NEGATIVE_AMOUNT, Currency.USD);
@@ -43,7 +43,7 @@ public class MoneyTest {
     // }
 
     @Test
-    public void constructorNullAmountParameterTest() {
+    void constructorNullAmountParameterTest() {
         assertThrows(NullPointerException.class, () -> {
             new Money(null, Currency.USD);
         });
@@ -60,21 +60,21 @@ public class MoneyTest {
 
     @ParameterizedTest
     @EnumSource(Currency.class)
-    public void constructorValidCurrenciesTest(Currency currency) {
+    void constructorValidCurrenciesTest(Currency currency) {
         Money money = new Money(DEFAULT_AMOUNT, currency);
         assertSame(DEFAULT_AMOUNT, money.getAmount());
         assertSame(currency, money.getCurrency());
     }
 
     @Test
-    public void addValidParameterTest() {
+    void addValidParameterTest() {
         Money money = new Money(DEFAULT_AMOUNT, Currency.USD);
         Money money2 = money.add(new Money(DEFAULT_AMOUNT, Currency.USD));
         assertEquals(money.getAmount().add(DEFAULT_AMOUNT), money2.getAmount());
     }
 
     @Test
-    public void addOtherCurrencyTest() {
+    void addOtherCurrencyTest() {
         Money money = new Money(DEFAULT_AMOUNT, Currency.USD);
         Money money2 = new Money(DEFAULT_AMOUNT, Currency.SEK);
         assertThrows(IllegalArgumentException.class, () -> {
@@ -83,7 +83,7 @@ public class MoneyTest {
     }
 
     @Test
-    public void addNullTest() {
+    void addNullTest() {
         Money money = new Money(DEFAULT_AMOUNT, Currency.USD);
         assertThrows(NullPointerException.class, () -> {
             money.add(null);
@@ -91,14 +91,14 @@ public class MoneyTest {
     }
 
     @Test
-    public void subtractValidParameterTest() {
+    void subtractValidParameterTest() {
         Money money = new Money(DEFAULT_AMOUNT, Currency.USD);
         Money money2 = money.subtract(new Money(DEFAULT_AMOUNT, Currency.USD));
         assertSame(money2.getAmount(), money.getAmount().subtract(DEFAULT_AMOUNT));
     }
 
     @Test
-    public void subtractOtherCurrencyTest() {
+    void subtractOtherCurrencyTest() {
         Money money = new Money(DEFAULT_AMOUNT, Currency.USD);
         Money money2 = new Money(DEFAULT_AMOUNT, Currency.SEK);
         assertThrows(IllegalArgumentException.class, () -> {
@@ -107,7 +107,7 @@ public class MoneyTest {
     }
 
     @Test
-    public void subtractNullTest() {
+    void subtractNullTest() {
         Money money = new Money(DEFAULT_AMOUNT, Currency.USD);
         assertThrows(NullPointerException.class, () -> {
             money.subtract(null);
