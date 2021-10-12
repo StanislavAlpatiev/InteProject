49bfb32f77fd21f722dff9d96303e9fc05efe394
verafied <verasolnygren@gmail.com>
2021-10-12 15:15:48 +0200

    Added item and itemtest classes

---
+++ RegisterSystem/src/test/java/se/su/dsv/RegisterSystem/ItemTest.java
@@ -0,0 +1,12 @@
+package se.su.dsv.RegisterSystem;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+    
+    @Test
+    void constructorValidTest(){
+        
+    }
+}
