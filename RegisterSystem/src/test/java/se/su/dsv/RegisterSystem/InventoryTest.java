484c8cd6c62ffb51e181321aa2dbda745fcc8a85
verafied <verasolnygren@gmail.com>
2021-10-13 16:58:52 +0200

    Added test stubs for InventoryTest

---
+++ RegisterSystem/src/test/java/se/su/dsv/RegisterSystem/InventoryTest.java
@@ -0,0 +1,64 @@
+package se.su.dsv.RegisterSystem;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+    
+    //Tests whether constructor without parameters works
+    @Test
+    void validConstructorEmptyParamTest(){
+
+    }
+
+    //Tests whether constructor with a list of items as parameter works
+    @Test
+    void validConstructorWithParamTest(){
+
+    }
+
+    //Tests whether constructor throws if fed with null or empty list of items
+    @Test
+    void constructorNullOrEmptyListParamThrowsTest(){
+
+    }
+
+    //Tests whether importing saved inventory works
+    @Test
+    void importItemsTest(){
+
+    }
+
+    //Tests whether importing broken saved inventory throws
+    @Test 
+    void brokenImportThrowsTest(){
+
+    }
+
+    //Tests whether export works as intended
+    @Test
+    void exportItemsTest(){
+
+    }
+
+    //Tests whether requested list of items can edit inventories list of items
+    @Test
+    void getItemsIsInmutableTest(){
+
+    }
+
+    //Tests whether item is flagged as available when it is
+    @Test
+    void itemIsAvailableTest(){
+
+    }
+
+    //Tests whether item is flagged as unavailable when it is
+    @Test
+    void itemIsNotAvailableTest(){
+
+    }
+
+
+
+}
