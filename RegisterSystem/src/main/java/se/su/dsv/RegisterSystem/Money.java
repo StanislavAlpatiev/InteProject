c96978b6e6c962a7d80e63ef427edf3bed8deb75
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2021-10-14 20:34:08 +0200

    reformated all code with intellij

--- RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Money.java
+++ RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Money.java
@@ -3,19 +3,19 @@ package se.su.dsv.RegisterSystem;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public class Money implements Comparable<Money>{
+public class Money implements Comparable<Money> {
 
     private BigDecimal amount;
     private final Currency currency;
 
 
-    public Money(BigDecimal amount, Currency currency){
+    public Money(BigDecimal amount, Currency currency) {
 
-        if(currency == null){
+        if (currency == null) {
             throw new IllegalArgumentException("currency is null");
         }
 
-        if(amount.compareTo(BigDecimal.ZERO) < 0){
+        if (amount.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("amount is negative");
         }
 
@@ -23,16 +23,16 @@ public class Money implements Comparable<Money>{
         this.currency = currency;
     }
 
-    public BigDecimal getAmount(){
+    public BigDecimal getAmount() {
         return amount;
     }
 
-    public Currency getCurrency(){
+    public Currency getCurrency() {
         return currency;
     }
 
-    public Money add(Money money){
-        if(!money.getCurrency().equals(this.currency)){
+    public Money add(Money money) {
+        if (!money.getCurrency().equals(this.currency)) {
             throw new IllegalArgumentException("mismatching currencies!");
         }
 
@@ -40,8 +40,8 @@ public class Money implements Comparable<Money>{
         return new Money(newAmount, this.currency);
     }
 
-    public Money subtract(Money money){
-        if(!money.getCurrency().equals(this.currency)){
+    public Money subtract(Money money) {
+        if (!money.getCurrency().equals(this.currency)) {
             throw new IllegalArgumentException("mismatching currencies!");
         }
 
@@ -52,13 +52,14 @@ public class Money implements Comparable<Money>{
 
     @Override
     public boolean equals(Object o) {
-        if (o == this)
+        if (o == this) {
             return true;
+        }
         if (!(o instanceof Money)) {
             return false;
         }
         Money money = (Money) o;
-        return Objects.equals(amount, money.amount) && Objects.equals(currency, money.currency); 
+        return Objects.equals(amount, money.amount) && Objects.equals(currency, money.currency);
     }
 
     @Override
@@ -68,7 +69,7 @@ public class Money implements Comparable<Money>{
 
     @Override
     public int compareTo(Money other) {
-        if(currency != other.currency) {
+        if (currency != other.currency) {
             return currency.compareTo(other.currency);
         }
         return amount.compareTo(other.amount);
