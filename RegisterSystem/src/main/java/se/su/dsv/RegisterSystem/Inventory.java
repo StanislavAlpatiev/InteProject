968990f596411d48b78d12fa89e00b0ed16193aa
verafied <verasolnygren@gmail.com>
2021-10-14 18:34:50 +0200

    Started implementing Inventory according to its tests

---
+++ RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Inventory.java
@@ -0,0 +1,42 @@
+package se.su.dsv.RegisterSystem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventory {
+
+    HashMap<Item, Integer> items;
+
+    public Inventory(){
+        items = new HashMap<>();
+    }
+
+    public Inventory(Item... item){
+        
+    }
+
+    public Map getItems(){
+
+    }
+
+    public boolean isAvailable(Item item){
+
+    }
+
+    public void importItems(){
+
+    }
+
+    public void importItems(String location){
+
+    }
+
+    public void exportItems(){
+
+    }
+
+    public void exportItems(String location){
+        
+    }
+
+}
