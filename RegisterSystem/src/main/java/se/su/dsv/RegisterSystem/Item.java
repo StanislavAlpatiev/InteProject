79c16f0ef653afdffb53128374ba241cc3d8fe51
palmoto1 <august.palm.music@gmail.com>
2021-10-14 16:42:02 +0200

    updated Reciept. Made item implement comparable

--- RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Item.java
+++ RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Item.java
@@ -1,6 +1,8 @@
 package se.su.dsv.RegisterSystem;
 
-public abstract class Item implements Vat {
+import java.math.BigDecimal;
+
+public abstract class Item implements Vat, Comparable<Item> {
 
     private Money price;
     private String name;
@@ -14,6 +16,35 @@ public abstract class Item implements Vat {
 
     public Money getPrice(){
         return price;
+    }
+
+    public Money getPricePlusVat(){
+        return price.add(getVATAmountOfPrice());
+    }
+
+    public Money getVATAmountOfPrice(){
+        BigDecimal vat = new BigDecimal(String.valueOf(getVAT()));
+        return new Money(price.getAmount().multiply(vat), price.getCurrency());
+
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        int cmp = name.compareTo(o.name);
+        if (cmp != 0)
+            return cmp;
+        return price.compareTo(o.price);
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o)
+            return true;
+        if (!(o instanceof Item))
+            return false;
+        Item other = (Item) o;
+        return name.equals(other.name) && price.equals(other.price);
+
     }
 
 }
--- RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Receipt.java
+++ RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Receipt.java
@@ -1,6 +1,7 @@
 package se.su.dsv.RegisterSystem;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
@@ -20,21 +21,62 @@ public class Receipt {
     }
 
     /**
-     * not complete
+     * unreadalble and too long, will split up in different methods
      */
     public String getReceipt() {
         StringBuilder sb = new StringBuilder();
         sb.append("==============================\n");
+
+        BigDecimal totalVat6 = new BigDecimal("0");
+        BigDecimal totalVat12 = new BigDecimal("0");
+        BigDecimal totalVat25 = new BigDecimal("0");
+        BigDecimal total = new BigDecimal("0");
+
         for (Map.Entry<Item, Integer> e : order.getItems().entrySet()) {
-            sb.append(e.getKey().getName()).append("        ");
+
+            String currentItemName = e.getKey().getName();
+            BigDecimal currentItemPrice = e.getKey().getPricePlusVat().getAmount().setScale(2, RoundingMode.CEILING);
+            int noOfItems = e.getValue();
+
+            sb.append(currentItemName).append("        ");
+
             if (e.getValue() > 1) {
-                sb.append(e.getValue()).append(" x ").append(e.getKey().getPrice().getAmount()).
-                        append("     ").append(e.getKey().getPrice().getAmount().multiply(new BigDecimal(e.getValue().toString()))).append("\n");
+                sb.append(e.getValue()).append(" x ").append(currentItemPrice).
+                        append("     ").append(currentItemPrice.multiply(new BigDecimal(noOfItems))).append("\n");
             } else
-                sb.append("       ").append(e.getKey().getPrice().getAmount()).append("\n");
+                sb.append("       ").append(currentItemPrice).append("\n");
+
+            total = total.add(currentItemPrice.multiply(new BigDecimal(noOfItems)));
+
+            if (e.getKey().getVAT() == 0.06)
+                totalVat6 = totalVat6.add(currentItemPrice.multiply(new BigDecimal(noOfItems)));
+            if (e.getKey().getVAT() == 0.12)
+                totalVat12 = totalVat12.add(currentItemPrice.multiply(new BigDecimal(noOfItems)));
+            if (e.getKey().getVAT() == 0.25)
+                totalVat25 = totalVat25.add(currentItemPrice.multiply(new BigDecimal(noOfItems)));
         }
         sb.append("==============================\n");
+        sb.append("TOTAL:            ").append(total.setScale(2, RoundingMode.CEILING).toString()).append("\n");
+        sb.append("Moms%    Moms     Netto     Brutto\n");
 
+        if (!totalVat6.toString().equals("0")) {
+            BigDecimal moms = totalVat6.multiply(new BigDecimal("0.06")).setScale(2, RoundingMode.CEILING);
+            BigDecimal netto = totalVat6.subtract(moms).setScale(2, RoundingMode.CEILING);
+            totalVat6 = totalVat6.setScale(2, RoundingMode.CEILING);
+            sb.append("6.00    ").append(moms.toString()).append("       ").append(netto.toString()).append("        ").append(totalVat6.toString()).append("\n");
+        }
+        if (!totalVat12.toString().equals("0")) {
+            BigDecimal moms = totalVat12.multiply(new BigDecimal("0.12")).setScale(2, RoundingMode.CEILING);
+            BigDecimal netto = totalVat12.subtract(moms).setScale(2, RoundingMode.CEILING);
+            totalVat12 = totalVat12.setScale(2, RoundingMode.CEILING);
+            sb.append("12.00    ").append(moms.toString()).append("     ").append(netto.toString()).append("      ").append(totalVat12.toString()).append("\n");
+        }
+        if (!totalVat25.toString().equals("0")) {
+            BigDecimal moms = totalVat25.multiply(new BigDecimal("0.25")).setScale(2, RoundingMode.CEILING);
+            BigDecimal netto = totalVat25.subtract(moms).setScale(2, RoundingMode.CEILING);
+            totalVat25 = totalVat25.setScale(2, RoundingMode.CEILING);
+            sb.append("25.00    ").append(moms.toString()).append("        ").append(netto.toString()).append("         ").append(totalVat25.toString()).append("\n");
+        }
         return sb.toString();
 
     }
