8f86edb55850d0db91206f0839f609256b359419
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2021-10-17 09:35:07 +0200

    Updated getWalletContent() to return a copy of walletContent istead of original reference

--- RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Wallet.java
+++ RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Wallet.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Wallet {
     private final Customer owner;
@@ -23,7 +24,8 @@ public class Wallet {
     }
 
     public Map<Currency, Money> getWalletContent() {
-        return walletContent;
+        return walletContent.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     public void add(Money money) {
