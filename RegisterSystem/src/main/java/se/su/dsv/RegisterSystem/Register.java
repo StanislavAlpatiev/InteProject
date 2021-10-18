2b0395013e426bb3f0820f794544d53296e30888
verafied <verasolnygren@gmail.com>
2021-10-18 14:07:28 +0200

    Added stub for Register tests

--- RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Register.java
+++ RegisterSystem/src/main/java/se/su/dsv/RegisterSystem/Register.java
@@ -3,11 +3,13 @@ package se.su.dsv.RegisterSystem;
 public class Register {
 
     private Currency currency;
+    private Inventory inventory;
     private static final Bank bank = new Bank();
 
 
     public Register(Currency currency){
         this.currency = currency;
+        inventory = new Inventory(bank, currency);
     }
 
     public Currency getCurrency(){
---
+++ RegisterSystem/src/test/java/se/su/dsv/RegisterSystem/RegisterTest.java
@@ -0,0 +1,5 @@
+package se.su.dsv.RegisterSystem;
+
+public class RegisterTest {
+    
+}
